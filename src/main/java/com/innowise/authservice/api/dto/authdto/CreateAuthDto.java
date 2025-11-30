26d55b1136dd4fc552bdedf77a035f9348be49d9
MegaXRL <zaxarrim@gmail.com>
2025-11-30 13:11:16 +0300

    "Prepare for delegate  reg with Gateway"

--- src/main/java/com/innowise/authservice/api/dto/authdto/CreateAuthDto.java
+++ src/main/java/com/innowise/authservice/api/dto/authdto/CreateAuthDto.java
@@ -1,13 +1,9 @@
 package com.innowise.authservice.api.dto.authdto;
 
-import java.time.*;
 import lombok.Data;
 
 @Data
 public class CreateAuthDto {
     private String email;
     private String password;
-    private String name;
-    private String surname;
-    private LocalDate birthDate;
 }
